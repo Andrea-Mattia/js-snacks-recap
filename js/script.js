080a99d25c975b041af6252a765a92a7df2fc43b
Andrea-Mattia <andrea.mattia94@gmail.com>
2021-05-01 18:38:56 +0200

    add snack 1

--- js/script.js
+++ js/script.js
@@ -330,6 +330,65 @@ const windowsPc = computers.filter(element => element.hasWindows === true).map((
 
 console.table(windowsPc);
 
+/*********************************************************************
+ * SNACK ESERCITAZIONE
+ *********************************************************************/
+
+console.log('*************** SNACK ESERCITAZIONE ***************');
+
+/**
+ SNACK 1
+A partire da un array di stringhe, crea un secondo array formattando le stringhe del primo array in minuscolo e con l'iniziale maiuscola.
+Es: ['pippo', 'PLUTO', 'Paperino'] => ['Pippo', 'Pluto', 'Paperino']
+*/
+
+console.log('********** SNACK 1 **********');
+
+const arr1 = ['pippo', 'PLUTO', 'Paperino'];
+
+const newArr = arr1.map((el) => {
+    el = el.toLowerCase();
+    return el.charAt(0).toUpperCase() + el.slice(1)
+});
+
+console.log(arr1);
+console.log(newArr);
+
+
+/*
+SNACK 2
+Un oggetto rappresenta una finestra di un browser e contiene due campi: 
+un array di tab aperte
+un numero che indica l'indice della tab aperta nell'array:
+{
+    "tab": ["Facebook", "GitHub", "Gmail"],
+    "activeTab": 0
+}
+Il software deve guardare se c'è un social aperto ed eliminarlo dalle tab.
+Nel caso la tab fosse attiva, deve attivare la successiva.
+*/
+
+
+
+
+
+/*
+SNACK 3  -> si potrebbe ottimizzare con una funzione separata per il capitalize
+A partire da un array di stringhe, crea un secondo array formattando le stringhe del primo array in minuscolo e con l'iniziale maiuscola.
+Es: ['pippo', 'PLUTO', 'Paperino'] => ['Pippo', 'Pluto', 'Paperino']
+*/
+
+
+
+
+
+/*
+SNACK 4 -> VUE 
+Partendo da un array di oggetti con i contatti di una rubrica con nome, cognome, numero, attivo (true,false)
+Andiamo a stampare nome cognome e numero dentro una lista.
+Fare in modo poi che i contatti non attivi (attivo: false) siano stampati di colore rosso mentre gli altri normalmente di nero. Cliccando sul numero della persona fate in modo che appaia in console il cognome della persona di cui è stato cliccato il numero
+ */
+
 
 
 
